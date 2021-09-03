9feebb8f938988f55a423de615e6f46ad690667a
FallJ <89920600+FallJ@users.noreply.github.com>
2021-09-03 18:52:25 -0400

    aligned temp next to forecast icon

--- Vanilla Weather/src/app.js
+++ Vanilla Weather/src/app.js
@@ -25,6 +25,7 @@ function displayTemperature(response) {
   let temperatureElement = document.querySelector("#temperature");
   let cityElement = document.querySelector("#city");
   let descriptionElement = document.querySelector("#description");
+
   let humidityElement = document.querySelector("#humidity");
   let windElement = document.querySelector("#wind");
   let dateElement = document.querySelector("#date");
