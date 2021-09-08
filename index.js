628f3eca58663a7e37035051e8b9bb3d8aa38395
FallJ <89920600+FallJ@users.noreply.github.com>
2021-09-08 09:57:52 -0400

    Used api to display forecast data

--- index.js
+++ index.js
@@ -21,25 +21,43 @@ function formatDate(timestamp) {
   return `${day} ${hours}:${minutes}`;
 }
 
+function formatDay(timestamp) {
+  let date = new Date(timestamp * 1000);
+  let day = date.getDay();
+  let days = ["Sun", "Mon", "Tue", "Wed", "Thu", "Fri", "Sat"];
+
+  return days[day];
+}
+
 function displayForecast(response) {
+  let forecast = response.data.daily;
+
   let forecastElement = document.querySelector("#forecast");
-  let days = ["Thu", "Fri", "Sat", "Sun"];
+
   let forecastHTML = `<div class="row">`;
-  days.forEach(function (day) {
-    forecastHTML =
-      forecastHTML +
-      `          <div class="col-2">
+  forecast.forEach(function (forecastDay, index) {
+    if (index < 6) {
+      forecastHTML =
+        forecastHTML +
+        `          <div class="col-2">
                <div class="weather-forecast-date">
-               ${day}
+               ${formatDay(forecastDay.dt)}
                </div>
-               <img src="http://openweathermap.org/img/wn/01d@2x.png" 
+               <img src="http://openweathermap.org/img/wn/${
+                 forecastDay.weather[0].icon
+               }@2x.png" 
                alt="sun" width="42" />
                <div class="weather-forecast-temperatures">
-               <span class="forecast-temperature-max">18°</span>
-                <span class="forecast-temperature-min">12°</span>
+               <span class="forecast-temperature-max">${Math.round(
+                 forecastDay.temp.max
+               )}°</span>
+                <span class="forecast-temperature-min">${Math.round(
+                  forecastDay.temp.min
+                )}°</span>
                </div>
              </div> 
             `;
+    }
   });
 
   forecastHTML = forecastHTML + `</div>`;
@@ -49,8 +67,7 @@ function displayForecast(response) {
 function getForecast(coordinates) {
   let apiKey = "a906446d03e88c127f9d85a4e9cd44be";
   let apiUrl = `https://api.openweathermap.org/data/2.5/onecall?lat=${coordinates.lat}&lon=${coordinates.lon}&appid=${apiKey}&units=metric`;
-axios.get(apiUrl).then(displayForecast);
-
+  axios.get(apiUrl).then(displayForecast);
 }
 
 function displayTemperature(response) {
@@ -120,4 +137,3 @@ let celsiusLink = document.querySelector("#celsius-link");
 celsiusLink.addEventListener("click", displayCelsiusTemperature);
 
 search("New York");
-
